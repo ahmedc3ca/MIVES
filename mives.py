c4629fc038bcfbae5fa395d0b33b5cd65196f46e
Ahmed Ezzo <ahmed.ezzo@epfl.ch>
2022-05-07 18:46:11 +0200

    checking weights == 1 (taking account extreme cases)

--- mives.py
+++ mives.py
@@ -17,6 +17,7 @@ from dialog import Ui_Dialog
 from Indicator_updated import *
 from values import Ui_ValuesWindow 
 import copy
+import math
 
 
 class Ui_MainWindow(QMainWindow):
@@ -378,7 +379,7 @@ class Ui_MainWindow(QMainWindow):
             summation = 0.0
             for child in tree.get_children():
                 summation = summation + float(self.weights[child.name])
-            if summation == 1:
+            if  math.isclose(summation, 1)::
                 check_children = True
                 for child in tree.get_children():
                     check_children = check_children and self.check_weights(child)
